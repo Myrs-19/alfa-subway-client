055b3feed0f4094be6952a4b844533087190ac09
Mikhail <my.rs@mail.ru>
2024-09-11 17:42:14 +0300

    constants

--- Scripts/constants.sql
+++ Scripts/constants.sql
@@ -1,8 +1,13 @@
 
 -- создание пакета, который хранит константы
 CREATE OR REPLACE PACKAGE CONSTANTS AS
-	dwi_title_lvl CONSTANT VARCHAR2 := 'dwi';
-	dws_title_lvl CONSTANT VARCHAR2 := 'dws';
-	stg_title_lvl CONSTANT VARCHAR2 := 'stg';
+	-- названия уровней staging dwh
+	dwi_title_lvl CONSTANT VARCHAR2(3 CHAR) := 'dwi';
+	dws_title_lvl CONSTANT VARCHAR2(3 CHAR) := 'dws';
+	stg_title_lvl CONSTANT VARCHAR2(3 CHAR) := 'stg';
 END;
 
+-- тест
+BEGIN
+	dbms_output.put_line(CONSTANTS.stg_title_lvl);
+END;
---
+++ Scripts/dwi/create_dwi_body_package.sql
@@ -0,0 +1,9 @@
+CREATE OR REPLACE PACKAGE BODY mike.dwi IS 
+	PROCEDURE wrap_dwi
+	IS 
+	BEGIN 
+		BEGIN -- для отлавливания ошибок
+			
+		END;
+	END;
+END;
