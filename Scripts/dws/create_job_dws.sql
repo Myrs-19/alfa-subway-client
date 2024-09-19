25c7eaf872871fc2f85790340bbea63fc366a7d7
Mikhail <my.rs@mail.ru>
2024-09-19 14:40:15 +0300

    создал джоб для dws и stg

--- Scripts/dws/create_job_dws.sql
+++ Scripts/dws/create_job_dws.sql
@@ -1,11 +1,11 @@
--- создание джоба для уровня dwi
+-- создание джоба для уровня dws
 BEGIN
-	DBMS_SCHEDULER.drop_job(job_name => 'dwi_job');
+	--DBMS_SCHEDULER.drop_job(job_name => 'dws_job');
 	
 	DBMS_SCHEDULER.CREATE_JOB (
-		job_name           =>  'dwi_job',
+		job_name           =>  'dws_job',
 		job_type           =>  'STORED_PROCEDURE',
-		job_action         =>  'mike.dwi.wrap_dwi',
+		job_action         =>  'mike.dws.wrap_dws',
 		repeat_interval    =>  'freq=minutely; interval=5; bysecond=0;', /* каждые 5 минут*/
 		comments           =>  'My new job',
 		enabled			   => TRUE
--- Scripts/stg/create_job_stg.sql
+++ Scripts/stg/create_job_stg.sql
@@ -1,11 +1,11 @@
--- создание джоба для уровня dwi
+-- создание джоба для уровня stg
 BEGIN
-	DBMS_SCHEDULER.drop_job(job_name => 'dwi_job');
+	--DBMS_SCHEDULER.drop_job(job_name => 'stg_job');
 	
 	DBMS_SCHEDULER.CREATE_JOB (
-		job_name           =>  'dwi_job',
+		job_name           =>  'stg_job',
 		job_type           =>  'STORED_PROCEDURE',
-		job_action         =>  'mike.dwi.wrap_dwi',
+		job_action         =>  'mike.stg.wrap_stg',
 		repeat_interval    =>  'freq=minutely; interval=5; bysecond=0;', /* каждые 5 минут*/
 		comments           =>  'My new job',
 		enabled			   => TRUE
